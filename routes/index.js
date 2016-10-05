1a0ebc1711529f37c7acc2b8bd6a9e7dbad5e436
shichiku_yuki <shichiku_yuki@cyberagent.co.jp>
2016-10-05 10:54:34 +0900

    Fixed method

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var request = require('superagent');
 var url = 'https://api.line.me/v2/bot/message/reply';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   var headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer E0IiIncd16SGcA5R9RS2Vgg/tu9VRhBtQLj/LGS7MGMPO7bmrhMOcRZ6cpU7Lj2kMYf910aWElRTtP3XA9+Rhv2wkguUrdBeA+NNCe+/QW3WP7P6FkgSWVMMe1oyQn4tShCSxkbhkx9MaZSUUB0wfgdB04t89/1O/w1cDnyilFU='
