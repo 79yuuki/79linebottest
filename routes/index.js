bc9dfd7dc0473b617ce08d404ae28f3bab71e56f
shichiku_yuki <shichiku_yuki@cyberagent.co.jp>
2016-10-05 10:50:32 +0900

    Fixed es5

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var router = express.Router();
-let request = require('superagent');
-let url = 'https://api.line.me/v2/bot/message/reply';
+var request = require('superagent');
+var url = 'https://api.line.me/v2/bot/message/reply';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let headers = {
+  var headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer E0IiIncd16SGcA5R9RS2Vgg/tu9VRhBtQLj/LGS7MGMPO7bmrhMOcRZ6cpU7Lj2kMYf910aWElRTtP3XA9+Rhv2wkguUrdBeA+NNCe+/QW3WP7P6FkgSWVMMe1oyQn4tShCSxkbhkx9MaZSUUB0wfgdB04t89/1O/w1cDnyilFU='
   };
 
   console.log(req);
 
-  let token
-  let message = 'default message';
-  let body = {
+  var token
+  var message = 'default message';
+  var body = {
     'replyToken': token,
     'messages': [
       {'type': 'text', 'text': message}
