51f5a45ada9f4d2fffad676ba34bc995030b9a84
shichiku_yuki <shichiku_yuki@cyberagent.co.jp>
2016-10-07 10:00:24 +0900

    Added reply API

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,13 @@ router.post('/', function(req, res, next) {
 
   console.log(req.body.events[0]);
 
-  var token
-  var message = 'default message';
+  var event = req.body.events[0];
+  if (event !== 'beacon') {
+    replyMessage('Beaconに近づいてね！', event);
+  }
+
+  var token = event.replyToken;
+  var message = 'Hello beacon!';
   var body = {
     'replyToken': token,
     'messages': [
@@ -21,15 +26,15 @@ router.post('/', function(req, res, next) {
     ]
   };
 
-  /*request.post(url)
-    .send(body)
-    .set('Content-Type', 'application/json')
-    .set('Authorization', 'Bearer E0IiIncd16SGcA5R9RS2Vgg/tu9VRhBtQLj/LGS7MGMPO7bmrhMOcRZ6cpU7Lj2kMYf910aWElRTtP3XA9+Rhv2wkguUrdBeA+NNCe+/QW3WP7P6FkgSWVMMe1oyQn4tShCSxkbhkx9MaZSUUB0wfgdB04t89/1O/w1cDnyilFU=')
-    .end((err, res) => {
-      console.log(err, res);
-    });*/
+  request.post(url)
+  .send(body)
+  .set('Content-Type', 'application/json')
+  .set('Authorization', 'Bearer E0IiIncd16SGcA5R9RS2Vgg/tu9VRhBtQLj/LGS7MGMPO7bmrhMOcRZ6cpU7Lj2kMYf910aWElRTtP3XA9+Rhv2wkguUrdBeA+NNCe+/QW3WP7P6FkgSWVMMe1oyQn4tShCSxkbhkx9MaZSUUB0wfgdB04t89/1O/w1cDnyilFU=')
+  .end((err, res) => {
+    console.log(err, res);
+  });
 
-  res.status(200);
+  //res.status(200);
 });
 
 module.exports = router;
